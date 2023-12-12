102807c8f14456c39cda09378d73b4009ded19d3
ayomidejoshua72 <ayomidejoshua72@gmail.com>
2023-12-12 10:12:38 +0100

    navigate automatically to login page

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -1,3 +1,7 @@
+import 'dart:async';
+
+import 'package:authentication_page/screens/login_screen.dart';
+
 import 'package:flutter/material.dart';
 
 class WelcomeScreen extends StatefulWidget {
@@ -8,8 +12,21 @@ class WelcomeScreen extends StatefulWidget {
 }
 
 class _WelcomeScreenState extends State<WelcomeScreen> {
-
-
+  @override
+  void initState() {
+    super.initState();
+    Timer(
+      const Duration(seconds: 20),
+      () {
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(
+            builder: (context) => const LoginScreen(),
+          ),
+        );
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
